28179a1a16cf4b9e7e1aafcd4ca3b8714afe8d30
Lost <pandagaminggr@gmail.com>
2024-09-19 18:14:17 +0300

    feat: implement getting all cards; creating a card; add axum-macros for better handler errors

--- services/axum-api/src/main.rs
+++ services/axum-api/src/main.rs
@@ -1,10 +1,14 @@
+mod card;
+
 use std::{sync::Arc, time::Duration};
 
-use axum::{error_handling::HandleErrorLayer, http::StatusCode, Router};
+use axum::{error_handling::HandleErrorLayer, http::StatusCode, routing::get, Router};
 use prisma::PrismaClient;
 use tower::{BoxError, ServiceBuilder};
 use tower_http::trace::TraceLayer;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+use card::{cards, create_card};
 
 #[derive(Debug, Clone)]
 struct AppState {
@@ -30,9 +34,15 @@ async fn main() {
         .expect("failed to create prisma client");
     // wrap client in Arc
     let client = Arc::new(client);
-
+    // create app state
+    let state = AppState {
+        prisma_client: client,
+    };
+    // create routers
+    let cards_router = Router::new().route("/", get(cards).post(create_card));
     // declare routes and respective handlers
     let app = Router::new()
+        .nest("/cards", cards_router)
         // add middleware for request timeout
         .layer(
             ServiceBuilder::new()
@@ -50,9 +60,7 @@ async fn main() {
                 .layer(TraceLayer::new_for_http())
                 .into_inner(),
         )
-        .with_state(AppState {
-            prisma_client: client,
-        });
+        .with_state(state);
 
     // declare listener
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
